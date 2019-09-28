f8a6c754725e22384bff0b9c80b5d4abdd33f400
Amit Maharana <amitmaharana@asu.edu>
2019-09-27 17:34:53 -0700

    Designed initial landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+      { path: 'register', component: UserRegistrationComponent },
+      { path: 'login', component: UserLoginComponent },   
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
