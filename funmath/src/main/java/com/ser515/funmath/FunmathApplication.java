f6fea0d8c76628d27bc413789f602bdca310e8ca
AsmiGandhi <aagandh3@asu.edu>
2019-09-27 20:12:50 -0700

    rest api code base

---
+++ funmath/src/main/java/com/ser515/funmath/FunmathApplication.java
@@ -0,0 +1,13 @@
+package com.ser515.funmath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunmathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunmathApplication.class, args);
+	}
+
+}
