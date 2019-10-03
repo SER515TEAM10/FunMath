79f1198d501dfd382d4479ed0ce724eff93a1efc
sharad dhar <sharaddhar@yahoo.com>
2019-10-03 00:20:44 -0700

    added functionality to register and delete users

--- funmath/src/main/java/com/ser515/funmath/FunmathApplication.java
+++ funmath/src/main/java/com/ser515/funmath/FunmathApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FunmathApplication {
+public class FunMathApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FunmathApplication.class, args);
+		SpringApplication.run(FunMathApplication.class, args);
 	}
 
 }
---
+++ funmath/src/main/java/com/ser515/funmath/services/UserService.java
@@ -0,0 +1,57 @@
+package com.ser515.funmath.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.ser515.funmath.model.Users;
+import com.ser515.funmath.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	public List<Users> findAll() {
+		return userRepository.findAll();
+	}
+
+	public Long getCount() {
+		return userRepository.count();
+	}
+
+	public Users saveUser(Users user) {
+		try {
+			return userRepository.saveAndFlush(user);
+			
+		} catch (Exception ex) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+					"Unable to add users at the moment, please try after sometime:" + ex.getMessage());
+		}
+	}
+
+	public Users findUserById(int id) {
+		// TODO Auto-generated method stub
+		System.out.println(id);
+		Optional<Users> user = userRepository.findById(id);
+		if (user.isPresent()) {
+			return user.get();
+		}
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find user with given id:" + id);
+	}
+
+	public void removeUser(int id) {
+		// TODO Auto-generated method stub
+		try {
+			userRepository.deleteById(id);
+		}catch(Exception ex) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to remove user with id:" + id+" - Error:"+ex.getMessage());
+		}
+		
+	}
+}
