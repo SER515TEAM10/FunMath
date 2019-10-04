a3251a458c0e2beeeaab17a7511ee75329587bcc
ddharne <ddharne@users.github.com>
2019-10-03 20:55:47 -0700

    updated the users

--- src/app/mock-users.ts
+++ src/app/mock-users.ts
@@ -4,11 +4,6 @@ export const Users: User[] = [
     { userid: 11, name: 'Asmi', password: 'Asmi', role: 0 },
     { userid: 12, name: 'Dhananjay', password: 'Dhananjay', role: 0 },
     { userid: 13, name: 'Sharad', password: 'Sharad', role: 0 },
-    { userid: 14, name: 'Amit', password: 'Amit', role: 0 },
-    { userid: 15, name: 'Satyen', password: 'Satyen', role: 0 },
-    { userid: 16, name: 'Asmi', password: 'Asmi', role: 0 },
-    { userid: 17, name: 'Dhananjay', password: 'Dhananjay', role: 0 },
-    { userid: 18, name: 'Sharad', password: 'Sharad', role: 0 },
-    { userid: 19, name: 'Amit', password: 'Amit', role: 0 },
-    { userid: 20, name: 'Satyen', password: 'Satyen', role: 0 }
+    { userid: 14, name: 'Naren', password: 'Naren', role: 0 },
+    { userid: 15, name: 'Satyen', password: 'Satyen', role: 0 }
 ];
