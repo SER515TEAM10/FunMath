05bc4ac35737381d4d43af8c21bddd9bf36d8db0
Satyen Verma <sverma44@asu.edu>
2019-11-03 14:54:49 -0700

    More changes to student grades page -- added comments column

--- src/app/student-grades/student-grades.component.ts
+++ src/app/student-grades/student-grades.component.ts
@@ -5,13 +5,14 @@ export interface GradedAssignments {
   position: number;
   marks: number;
   total: number;
+  comments: string;
 }
 
 const GRADES_DATA: GradedAssignments[] = [
-  {position: 1, name: 'Assignment1', marks: 90, total: 100},
-  {position: 2, name: 'Assignment2', marks: 90, total: 100},
-  {position: 3, name: 'Assignment3', marks: 90, total: 100},
-  {position: 4, name: 'Assignment4', marks: 90, total: 100}
+  {position: 1, name: 'Addition Assignment', marks: 100, total: 100, comments: 'Good job'},
+  {position: 2, name: 'Subtraction Assignment', marks: 60, total: 100, comments: 'Work harder next time'},
+  {position: 3, name: 'Multiplication Assignment', marks: 90, total: 100, comments: 'Nice'},
+  {position: 4, name: 'Division Assignment', marks: 100, total: 100, comments: 'Amazing'}
 ];
 
 @Component({
@@ -21,7 +22,7 @@ const GRADES_DATA: GradedAssignments[] = [
 })
 export class StudentGradesComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'marks', 'total'];
+  displayedColumns: string[] = ['position', 'name', 'marks', 'total', 'comments'];
   dataSource = GRADES_DATA;
   constructor() { }
 
