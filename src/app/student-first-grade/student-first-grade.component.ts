25d83f0118841896dac924f4de374c8796a77c73
Sharad Dhar <42695310+sdhar1811@users.noreply.github.com>
2019-11-16 13:34:15 -0700

    US#6 student first grade  - populate questions dynamically

--- src/app/student-first-grade/student-first-grade.component.ts
+++ src/app/student-first-grade/student-first-grade.component.ts
@@ -6,17 +6,203 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-first-grade.component.scss']
 })
 export class StudentFirstGradeComponent implements OnInit {
+  public i: number = 0;
+  public question: any;
+  public countingQuestions = [
+    {
+      number: "1",
+      desc: "Count number of balls",
+      images: [
+        {
+          name: "counting/ball2.jpg"
+        },
+        {
+          name: "counting/ball2.jpg"
+        },
+        {
+          name: "counting/ball2.jpg"
+        },
+        {
+          name: "counting/ball2.jpg"
+        }
+      ],
+      answers: {
+        type: "button",
+        buttons: [
+          {
+            id: "1",
+            name: "",
+            image: "counting/1.jpg",
+            desc: ""
+          },
+          {
+            id: "2",
+            name: "",
+            image: "counting/2.jpg",
+            desc: ""
+          },
+          {
+            id: "3",
+            name: "",
+            image: "counting/3.jpg",
+            desc: ""
+          }
+        ]
+      }
+    },
+    {
+      number: "2",
+      desc: "Count number of doughnuts",
+      images: [
+        {
+          name: "counting/doughnut.png"
+        },
+        {
+          name: "counting/doughnut.png"
+        },
+        {
+          name: "counting/doughnut.png"
+        },
+        {
+          name: "counting/doughnut.png"
+        },
+        {
+          name: "counting/doughnut.png"
+        },
+        {
+          name: "counting/doughnut.png"
+        }
+      ],
+      answers: {
+        type: "button",
+        buttons: [
+          {
+            id: "1",
+            name: "",
+            image: "counting/1.jpg",
+            desc: ""
+          },
+          {
+            id: "2",
+            name: "",
+            image: "counting/2.jpg",
+            desc: ""
+          },
+          {
+            id: "3",
+            name: "",
+            image: "counting/3.jpg",
+            desc: ""
+          }
+        ]
+      }
+    }
+  ]
+  public patternQuestions = [
+    {
+      number: "1",
+      desc: "What number comes next in the given pattern?",
+      images: [
+        {
+          name: "counting/1.jpg"
+        },
+        {
+          name: "counting/2.jpg"
+        },
+        {
+          name: "counting/2.jpg"
+        },
+        {
+          name: "counting/1.jpg"
+        },
+        {
+          name: "counting/2.jpg"
+        },
+        {
+          name: "counting/2.jpg"
+        },
+        {
+          name: "counting/qmark.jpg"
+        }
+      ],
+      answers: {
+        type: "image",
+        buttons: [
+          {
+            id: "1",
+            name: "",
+            image: "counting/1.jpg",
+            desc: ""
+          },
+          {
+            id: "2",
+            name: "",
+            image: "counting/2.jpg",
+            desc: ""
+          }
+        ]
+      }      
+    },
+    {
+      number: "2",
+      desc: "What number comes next in the given pattern?",
+      images: [
+        {
+          name: "counting/5.jpg"
+        },
+        {
+          name: "counting/1.jpg"
+        },
+        {
+          name: "counting/6.jpg"
+        },
+        {
+          name: "counting/5.jpg"
+        },
+        {
+          name: "counting/1.jpg"
+        },
+        {
+          name: "counting/6.jpg"
+        },
+        {
+          name: "counting/5.jpg"
+        },
+        {
+          name: "counting/1.jpg"
+        },
+        {
+          name: "counting/qmark.jpg"
+        }
+      ],
+      answers: {
+        type: "image",
+        buttons: [
+          {
+            id: "1",
+            name: "",
+            image: "counting/1.jpg",
+            desc: ""
+          },
+          {
+            id: "2",
+            name: "",
+            image: "counting/2.jpg",
+            desc: ""
+          }
+        ]
+      }
+    }
 
-  displayCounting: boolean
-  displayPatterns: boolean
+  ]
+
+  test_for_pattern = false; 
   buttonselected: string
   labelstringC1S1Q1: string
   labelstringC1S1Q2: string
   labelstringC1S7Q1: string
   labelstringC1S7Q2: string
-  constructor() {
-    this.displayCounting = false;
-    this.displayPatterns = false;
+  constructor() {   
     this.labelstringC1S1Q1 = "";
     this.labelstringC1S1Q2 = "";
     this.labelstringC1S7Q1 = "";
@@ -26,13 +212,14 @@ export class StudentFirstGradeComponent implements OnInit {
   ngOnInit() {
   }
   countingProblems(): void {
-    this.displayCounting = !this.displayCounting;
-    this.displayPatterns = false;
-
+    this.test_for_pattern = true;
+    this.question = this.countingQuestions;
+    this.i = 0;
   }
   patternProblems(): void {
-    this.displayPatterns = !this.displayPatterns;
-    this.displayCounting = false;
+    this.test_for_pattern = true;
+    this.question = this.patternQuestions;
+    this.i = 0;
   }
 
 
@@ -75,5 +262,13 @@ export class StudentFirstGradeComponent implements OnInit {
     }
     return this.labelstringC1S7Q2;
   }
+  // goToNextQuestion(questionNumber: string) {
+  //   if (questionNumber == "displayPattern_2") {
+  //     this.displayPatterns_2 = true;
+  //   } else if (questionNumber == "displayCounting_2") {
+  //     this.displayCounting_2 = true;
+  //   }
+
+  // }
 
 }
