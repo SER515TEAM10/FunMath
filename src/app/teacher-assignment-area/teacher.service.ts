6704f9ecc6680940f8091c68185a6450667789a8
AsmiGandhi <aagandh3@asu.edu>
2019-11-19 00:38:34 -0700

    US7: added teacher-service

--- src/app/teacher-assignment-area/teacher.service.ts
+++ src/app/teacher-assignment-area/teacher.service.ts
@@ -1,40 +1,34 @@
-import { Injectable } from '@angular/core';
-<<<<<<< HEAD
-=======
-import { HttpHeaders, HttpClient} from '@angular/common/http';
-import {Observable, of} from 'rxjs'
-import { QuestionModel } from './question-model';
-import { catchError, map, tap } from 'rxjs/operators';
-
->>>>>>> 4c45ac3411fab45611816a34d5d0549cd0e7b3f4
+import { Injectable } from "@angular/core";
+import { HttpHeaders, HttpClient } from "@angular/common/http";
+import { Observable, of } from "rxjs";
+import { QuestionModel } from "./question-model";
+import { catchError, map, tap } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class TeacherService {
-<<<<<<< HEAD
-
-  constructor() { }
-=======
-  private teacherUrl = 'http://localhost:8080/user'; 
+  private teacherUrl = "http://localhost:8080/user";
   httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    headers: new HttpHeaders({ "Content-Type": "application/json" })
   };
 
-  constructor(private http:HttpClient) { } 
+  constructor(private http: HttpClient) {}
 
-  getQuestionByClassCategory(classNumber:number,category:string): Observable<QuestionModel> {
+  getQuestionByClassCategory(
+    classNumber: number,
+    category: string
+  ): Observable<QuestionModel> {
     const url = `${this.teacherUrl}/question/getByClassCategory`;
-    const body = {"classNumber":classNumber,"category":category};
-    return this.http.post<QuestionModel>(url,body).pipe(
+    const body = { classNumber: classNumber, category: category };
+    return this.http.post<QuestionModel>(url, body).pipe(
       tap(_ => console.log(`questions fetched=`)),
       catchError(this.handleError<QuestionModel>(`getAllQuestions`))
     );
   }
 
-  private handleError<T>(operation = 'operation', result?: T) {    
+  private handleError<T>(operation = "operation", result?: T) {
     return (error: any): Observable<T> => {
-
       // TODO: send the error to remote logging infrastructure
       //console.error(error); // log to console instead
 
@@ -45,6 +39,4 @@ export class TeacherService {
       return of(result as T);
     };
   }
-
->>>>>>> 4c45ac3411fab45611816a34d5d0549cd0e7b3f4
 }
