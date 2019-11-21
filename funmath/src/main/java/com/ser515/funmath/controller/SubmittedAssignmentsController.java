fd2dceae391a4b528ac54b5b7632855e34987dbb
Satyen Verma <sverma44@asu.edu>
2019-11-20 21:31:29 -0700

    US #59 Task #66, added SubmittedAssignment Service and related changes

---
+++ funmath/src/main/java/com/ser515/funmath/controller/SubmittedAssignmentsController.java
@@ -0,0 +1,27 @@
+package com.ser515.funmath.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ser515.funmath.model.SubmittedAssignments;
+import com.ser515.funmath.services.SubmitAssignmentService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/submittedassignments")
+public class SubmittedAssignmentsController {
+
+	@Autowired
+	SubmitAssignmentService submitAssignmentService;
+
+	@GetMapping("/search/{emailId}")
+	public List<SubmittedAssignments> findGradesByEmail(@PathVariable String emailId) {
+		return submitAssignmentService.findGrades(emailId);
+	}
+}
